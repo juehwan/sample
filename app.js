0c4cce93930253643e4949049fb65dae42e6c5e6
bg0077@naver.com <bg0077@naver.com>
2018-03-23 16:01:19 +0900

    e

---
+++ app.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+
+var b = document.getElementById("box2");
+    b.style.backgroundColor = "blue";
+    b.onmouseover = function(){
+        this.sytle.backgroundColor = "yellow";
+        }
+        b.onmouseleave = function(){
+            this.style.backgroundColor = "skyblue";
+        }
+    }
+
